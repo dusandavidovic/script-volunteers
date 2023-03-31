e60a5d9eac1a022a98402e5a43ca00beb81a99be
Dusan <dusandavidovic@gmail.com>
2023-03-31 19:47:19 -0400

    changes to apiclient

--- src/apiclient.js
+++ src/apiclient.js
@@ -1,13 +1,14 @@
 function doGet(e) {
   if (typeof e !== "undefined") {
     console.log("api-client doGet...parameters", JSON.stringify(e.parameters));
+    const data = getEventSubscriptions(e.parameter.mode);
 
-    if (e.parameters.action === WEB_API.actions.event) {
-    } else {
-      if (e.parameters.action === WEB_API.actions.getEventSubscriptions) {
-        const data = getEventSubscriptions(e.parameter.id, e.parameter.sheet);
-      }
-    }
+    // if (e.parameters.action === WEB_API.actions.event) {
+    // } else {
+    //   if (e.parameters.action === WEB_API.actions.getEventSubscriptions) {
+    //     const data = getEventSubscriptions(e.parameter.mode);
+    //   }
+    // }
 
     return ContentService.createTextOutput(JSON.stringify(data));
 
